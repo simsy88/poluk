36fcfffe908983a4010962ec4dc984fb135586b5
simsy88 <andrew_sims1@hotmail.com>
2020-05-03 16:24:38 +0100

    Allow alliance to accept differing levels of compliance

--- poluk/alliance.py
+++ poluk/alliance.py
@@ -2,11 +2,17 @@ import numpy as np
 from poluk.forecast import recalculate
 
 
-def alliance(df, col, adherence):
+def alliance(df, col, attractiveness):
 
     members = [p for p in set(df[col]) if p is not None]
 
     for chosen in members:
+
+        if type(attractiveness) == dict:
+            adherence = attractiveness[chosen]
+        else:
+            adherence = attractiveness
+
         df[chosen+'_pct'] = np.where(df[col] == chosen,
                                      df[chosen+'_pct'] + df[[p+'_pct' for p in members if p != chosen]
                                                             ].sum(axis=1)*adherence,
